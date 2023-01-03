c561cf612a88e6a1707a3cab55677564685a8cf9
Louis_Grant <ljsgrant@gmail.com>
2023-01-03 17:08:08 +0000

    updated post model with comments & replies, updated commentsController with createComment function

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -1,4 +1,30 @@
-async function createComment(req, res, next) {}
+import mongoose from 'mongoose';
+import Post from '../models/post.js';
+
+async function createComment(req, res, next) {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res
+        .status(404)
+        .send({ message: `Couldn't find post with id ${req.params.id}` });
+    }
+
+    const newComment = {
+      ...req.body,
+      addedBy: req.currentUser._id
+    };
+
+    post.comments.push(newComment);
+
+    const updatedPost = await post.save();
+
+    return res.status(201).json(updatedPost);
+  } catch (error) {
+    next(error);
+  }
+}
 
 async function updateComment(req, res, next) {}
 
--- models/post.js
+++ models/post.js
@@ -1,12 +1,20 @@
 import mongoose from 'mongoose';
 
+const replySchema = new mongoose.Schema({
+  text: { type: String, required: true, min: 1, max: 500 },
+  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+  timestamp: { type: String },
+  likes: { type: Number },
+  dislikes: { type: Number },
+});
+
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true, min: 1, max: 500 },
   addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   timestamp: { type: String },
   likes: { type: Number },
   dislikes: { type: Number },
-  replies: [commentSchema]
+  replies: [replySchema]
 });
 
 const postSchema = new mongoose.Schema({
