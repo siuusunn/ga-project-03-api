ce5085208e3a9b98168fe4cab98f664cc86fe854
Louis_Grant <ljsgrant@gmail.com>
2023-01-04 11:01:27 +0000

    working on fixes to commentscontroller; added secureRoute to make new post

--- config/router.js
+++ config/router.js
@@ -3,11 +3,13 @@ import commentsController from '../controllers/commentsController.js';
 import postsController from '../controllers/postsController.js';
 import usersController from '../controllers/usersController.js';
 
+import secureRoute from '../middleware/secureRoute.js';
+
 const Router = express.Router();
 
 Router.route('/posts')
   .get(postsController.getAllPosts)
-  .post(postsController.createNewPost);
+  .post(secureRoute, postsController.createNewPost);
 
 Router.route('/posts/:id')
   .get(postsController.getSinglePost)
