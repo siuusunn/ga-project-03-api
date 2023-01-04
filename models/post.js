a7ff13c1b169e11f1c1f30965cde7f4f8f33e345
Louis_Grant <ljsgrant@gmail.com>
2023-01-04 10:07:37 +0000

    update post schema with comments schema

--- models/post.js
+++ models/post.js
@@ -1,28 +1,21 @@
 import mongoose from 'mongoose';
 
-const replySchema = new mongoose.Schema({
-  text: { type: String, required: true, min: 1, max: 500 },
-  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  timestamp: { type: String },
-  likes: { type: Number },
-  dislikes: { type: Number },
-});
-
 const commentSchema = new mongoose.Schema({
   text: { type: String, required: true, min: 1, max: 500 },
   addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   timestamp: { type: String },
-  likes: { type: Number },
-  dislikes: { type: Number },
-  replies: [replySchema]
+  likes: { type: Number, min: 0, default: 0 },
+  dislikes: { type: Number, min: 0, default: 0 }
 });
+
+commentSchema.add({ comments: [commentSchema] });
 
 const postSchema = new mongoose.Schema({
   text: { type: String, required: true, min: 1, max: 1000 },
   addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
   timestamp: { type: String },
-  likes: { type: Number },
-  dislikes: { type: Number },
+  likes: { type: Number, min: 0, default: 0 },
+  dislikes: { type: Number, min: 0, default: 0 },
   comments: [commentSchema]
 });
 
