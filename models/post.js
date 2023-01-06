41a56fabf7df90d284dd36e97b7e6b4f48aebc22
Louis_Grant <ljsgrant@gmail.com>
2023-01-06 01:51:29 +0000

    add timestamp for comments

--- models/post.js
+++ models/post.js
@@ -1,17 +1,20 @@
 import mongoose from 'mongoose';
 
-const commentSchema = new mongoose.Schema({
-  text: { type: String, required: true, min: 1, max: 500 },
-  addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
-  timestamp: { type: String },
-  likes: { type: Number, min: 0, default: 0 },
-  dislikes: { type: Number, min: 0, default: 0 },
-  parentPostId: { type: mongoose.Schema.ObjectId, ref: 'Post' },
-  parentCommentId: { type: mongoose.Schema.ObjectId, ref: 'Comment' },
-  comments: [{ type: mongoose.Schema.ObjectId, ref: 'Comment' }],
-  deletedComments: [{ type: mongoose.Schema.ObjectId }],
-  isDeleted: { type: Boolean, default: false }
-});
+const commentSchema = new mongoose.Schema(
+  {
+    text: { type: String, required: true, min: 1, max: 500 },
+    addedBy: { type: mongoose.Schema.ObjectId, ref: 'User', required: true },
+    timestamp: { type: String },
+    likes: { type: Number, min: 0, default: 0 },
+    dislikes: { type: Number, min: 0, default: 0 },
+    parentPostId: { type: mongoose.Schema.ObjectId, ref: 'Post' },
+    parentCommentId: { type: mongoose.Schema.ObjectId, ref: 'Comment' },
+    comments: [{ type: mongoose.Schema.ObjectId, ref: 'Comment' }],
+    deletedComments: [{ type: mongoose.Schema.ObjectId }],
+    isDeleted: { type: Boolean, default: false }
+  },
+  { timestamps: true }
+);
 
 const postSchema = new mongoose.Schema(
   {
